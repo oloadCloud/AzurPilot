b1aee176808879df60290c5d6a5ae8f1ea0775d8
oc <491514064@qq.com>
2026-06-10 06:01:10 +0800

    自用14-2

--- campaign/campaign_main/campaign_14_2.py
+++ campaign/campaign_main/campaign_14_2.py
@@ -70,14 +70,18 @@ class Campaign(CampaignBase):
     def battle_0(self):
         # 14-2 do not have light house
 
-        if not self.picked_flare and H7.is_accessible and A5.is_accessible:
-            self.fleet_boss.pick_up_flare(H7)
-            self.fleet_boss.pick_up_flare(A5)
-            self.fleet_boss.goto(D6)
-            self.fleet_1.switch_to()
+        if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=1):
+            return True
 
+        return self.battle_default()
+
+    def battle_3(self):
+        if not self.picked_flare and H7.is_accessible and A5.is_accessible:
+            self.pick_up_flare(H7)
+            self.pick_up_flare(A5)
         if self.clear_roadblocks([road_A5, road_H7], weakest=True):
             return True
+            
         if self.clear_filter_enemy(self.ENEMY_FILTER, preserve=1):
             return True
 
