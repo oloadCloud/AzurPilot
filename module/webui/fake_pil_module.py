fb5ae911518343a1a77ed7a14f11e8636c69d0d5
oc <491514064@qq.com>
2026-08-16 03:17:55 +0800

    更新骗术
    新增 UnidentifiedImageError ，适配 adbutils 1.2.3 ，用于修复导入异常。

--- module/webui/fake_pil_module.py
+++ module/webui/fake_pil_module.py
@@ -13,6 +13,7 @@ def import_fake_pil_module():
     fake_pil_module = ModuleType('PIL')
     fake_pil_module.Image = ModuleType('PIL.Image')
     fake_pil_module.Image.Image = type('MockPILImage', (), dict(__init__=None))
+    fake_pil_module.UnidentifiedImageError = type('MockUnidentifiedImageError', (Exception,), {})
     sys.modules['PIL'] = fake_pil_module
     sys.modules['PIL.Image'] = fake_pil_module.Image
 
@@ -20,3 +21,4 @@ def import_fake_pil_module():
 def remove_fake_pil_module():
     sys.modules.pop('PIL', None)
     sys.modules.pop('PIL.Image', None)
+    sys.modules.pop('PIL.UnidentifiedImageError', None)
